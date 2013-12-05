5620f6b7c5c5abfd5541d1581a73d5f21860436a
thanmann <timhanmann@yahoo.com>
2013-12-04 23:26:15 -0800

    Updated
    added level menu

--- Project 2/Labyrinth/main.cpp
+++ Project 2/Labyrinth/main.cpp
@@ -7,8 +7,16 @@
 #include <windows.h>
 #include <iostream>
 #include <ctime>
+#include <fstream>
 #include <string>
 #include <cstdlib>
+//Function from Joshua Camacho don't credit for line count
+#ifdef _WIN32
+#endif
+#ifdef _WIN32
+static HANDLE                     hConsoleOutput = 0;
+static CONSOLE_SCREEN_BUFFER_INFO csbi;
+#endif
 using namespace std;
 
 //Global Constants
@@ -16,9 +24,9 @@ const char WIDTH = 30, HEIGHT = 30;
 
 
 //Function Prototypes
-unsigned char map1[WIDTH][HEIGHT]=  
+unsigned char map1[HEIGHT][WIDTH]=  
                    {"############################",
-                    "#    #   *  #              #",
+                    "#@   #   *  #              #",
                     "# #   ######  #### ####### #",
                     "# #*   #       #    ##     #",
                     "# ###  #  ######*    #*  # #",
@@ -28,95 +36,93 @@ unsigned char map1[WIDTH][HEIGHT]=
                     "# #########*    #  ####  # #",
                     "#            #  #*      *# #",
                     "############## *########## #",
-                    "#..........................#",
-                    "#..........................#",
-                    "#..........................#",
+                    "#                          #",
+                    "#                          #",
+                    "#                          #",
                     "############################"};
 
-unsigned char currentMap[WIDTH][HEIGHT];
+unsigned char currentMap[HEIGHT][WIDTH];
 void printMap(int, int);
-bool stopGame(bool,int,int,int,int);
-int NumKey(int,int,int,int);
-char player = '@';
-int playerX = 1, playerY = 1;
+void clear();
+//char player = '@';
+//int playerX = 1, playerY = 1;
+int timer(int, int);
+void LevelMenu();
 
 int main(int argc, char** argv) {
     
     //Declare variables
-    int Level = 1;
-    int width, height;
-    int left, right,up,down;
+    int minutes,seconds;
+    string newPlayer;
+    int width, height,userinput ;
+    int left,right,up,down;
     bool gameOver = false;
+    int HP=0, MaxHp=100, gamespeed=100;
+    char ans, choice;
+    
+    //Opens up file to save/load Times
+    ofstream fout;
+    fout.open("Save.dat", ios::app);
+    
+    ifstream infile;
+    infile.open ("Save.dat");
     
     
-    printMap(width, height);
+    do{
+        // Welcome/Explanation on Labyrinth
+    cout << "The point of Labyrinth is to reach the end of the maze" << endl
+                << "while avoiding obstacles and keeping your HP above 0."
+                << endl << endl << endl;
+    cout << "Are you new to the game?" << endl;
+    cin >> choice;
+    if (choice == 'Y' || choice == 'y'){
+        cout << "What is your first name?" << endl;
+        cin >> newPlayer;
+        fout << newPlayer << endl;
+        LevelMenu();
+        cin >> userinput;
+        
+    } else{
+           LevelMenu();
+           cin >> userinput;
+}          
+                   
     
-    
-   // ArrowKey(left, right, up, down);
-    
-    //stopGame(gameOver, left, right,up, down);
 
-  while(gameOver == false){
-      currentMap[playerX][playerY] = player;
-      if (GetAsyncKeyState (VK_NUMPAD8)){
-          currentMap[playerX][playerY + 1];
-          playerY++;   
-      } if (GetAsyncKeyState (VK_NUMPAD2)){
-          currentMap[playerX][playerY - 1];
-          playerY--;   
-      } if (GetAsyncKeyState (VK_NUMPAD6)){
-          currentMap[playerX + 1][playerY];
-          playerX++;   
-      } if (GetAsyncKeyState (VK_NUMPAD4)){
-          currentMap[playerX - 1][playerY];
-          playerX--;   
-      }
-  }
     
-   return 0;
-}
-void printMap(int x, int y){
-    system("CLS");
-   for(x = 0;x < WIDTH;x++){
-        for( y = 0; y < HEIGHT;y++){
-            currentMap[x][y] = map1[x][y];
+    
+while (gameOver == false && choice == 1){
+        for( int y=0;y<30;y++){
+           cout << map1[y];
         }
-    }
-  
-  currentMap[playerX][playerY] = player;
-
-for(x = 0; x < WIDTH;x++){
-        cout << endl;
-        for(y = 0;y < HEIGHT;y++){
-            cout << currentMap[x][y];
-        }
-    }  
-}
-
-/*int ArrowKey(int x, int y, int Level, int HP){
- switch(currentMap[x][y]){
+      clear();
+      //timer(minutes,seconds);
+    for(int y = 0;y < 30;y++){
+        for(int x = 0; x < 30;x++){
+            
+            switch(map1[y][x]){
                   case '#':{
                       map1[y][x] = 219;
                   }
               case '@':
               { //Arrow Mapping for Up Key
                 if (GetAsyncKeyState (VK_UP) != 0){                    
-                     int y2 = (y-1);
+                     int y2 = (y+1);
                   
                   switch (map1[y2][x]){
                          case ' ':
                               {
                                   map1[y][x] = ' ';
-                                  y -= 1;
+                                  y += 1;
                                   map1[y2][x]= '@';
                                   }break;
                       case '!':{
-                          Level = 2;
+                          gameOver=true;
                       }break;
                       case '*':{//Loss of HP
                                   HP -= 20;
                                   map1[y][x] = ' ';
-                                  y -= 1;
+                                  y += 1;
                                   map1[y2][x]= '@';
                       }break;
                          }
@@ -132,7 +138,7 @@ for(x = 0; x < WIDTH;x++){
                                  map1[y2][x]= '@'; 
                         }break;
                         case '!':{
-                            Level = 2;
+                            gameOver=true;
                         }break;
                         case '*':{//Loss of HP
                                   HP -= 20;
@@ -152,7 +158,7 @@ for(x = 0; x < WIDTH;x++){
                                  map1[y][x2]= '@';
                         }break;
                         case '!':{
-                            Level = 2;
+                            gameOver=true;
                         }break;
                         case '*':{//Loss of HP
                                   HP -= 20;
@@ -172,101 +178,7 @@ for(x = 0; x < WIDTH;x++){
                                   map1[y][x2]= '@';
                         }break;
                         case '!':{
-                            Level = 2;
-                        }break;
-                        case '*':{//Loss of HP
-                                  HP -= 20;
-                                  map1[y][x] = ' ';
-                                  x -= 1;
-                                  map1[y][x2]= '@';
-                        }break;
-                    }
-                }
-                
-                }break;
-             }   
-}*/
-bool stopGame(bool go, int x, int y, int HP, int Level){
-    while(go == false){
-        switch(currentMap[x][y]){
-                  case '#':{
-                      map1[y][x] = 219;
-                  }
-              case '@':
-              { //Arrow Mapping for Up Key
-                if (GetAsyncKeyState (VK_UP) != 0){                    
-                     int y2 = (y-1);
-                  
-                  switch (map1[y2][x]){
-                         case ' ':
-                              {
-                                  map1[y][x] = ' ';
-                                  y -= 1;
-                                  map1[y2][x]= '@';
-                                  }break;
-                      case '!':{
-                          Level = 2;
-                      }break;
-                      case '*':{//Loss of HP
-                                  HP -= 20;
-                                  map1[y][x] = ' ';
-                                  y -= 1;
-                                  map1[y2][x]= '@';
-                      }break;
-                         }
-                   }//Arrow Mapping for Down Key
-                if(GetAsyncKeyState (VK_DOWN) != 0){
-                    
-                    int y2 = (y + 1);
-                    
-                    switch(map1[y2][x]){
-                        case ' ':{
-                                 map1[y][x] = ' ';
-                                 y += 1;
-                                 map1[y2][x]= '@'; 
-                        }break;
-                        case '!':{
-                            Level = 2;
-                        }break;
-                        case '*':{//Loss of HP
-                                  HP -= 20;
-                                  map1[y][x] = ' ';
-                                  y += 1;
-                                  map1[y2][x]= '@';
-                        }break;
-                    }
-                } //Arrow Mapping for Right Key
-                if (GetAsyncKeyState (VK_RIGHT) != 0){
-                    int x2 = (x + 1);
-                    
-                    switch(map1[y][x2]){
-                        case ' ':{
-                                 map1[y][x] = ' ';
-                                 x += 1;
-                                 map1[y][x2]= '@';
-                        }break;
-                        case '!':{
-                            Level = 2;
-                        }break;
-                        case '*':{//Loss of HP
-                                  HP -= 20;
-                                  map1[y][x] = ' ';
-                                  x += 1;
-                                  map1[y][x2]= '@';
-                        }break;
-                    }
-                } //Arrow Mapping for Left Key
-                if (GetAsyncKeyState (VK_LEFT) !=0){
-                    int x2 = (x - 1);
-                    
-                    switch (map1[y][x2]){
-                        case ' ':{
-                                  map1[y][x] = ' ';
-                                  x -= 1;
-                                  map1[y][x2]= '@';
-                        }break;
-                        case '!':{
-                            Level = 2;
+                           gameOver=true;
                         }break;
                         case '*':{//Loss of HP
                                   HP -= 20;
@@ -279,5 +191,77 @@ bool stopGame(bool go, int x, int y, int HP, int Level){
                 
                 }break;
              }
+         }
+       }
+       
+    Sleep(gamespeed);
+    }cout << "Would you like to play the game again?" << endl;
+    cin >> ans;
+ }while (ans == 'Y' || ans == 'y');
+   system("PAUSE");
+   return EXIT_SUCCESS;
+}
+
+void LevelMenu(){
+     cout << "Which map would you like to choose?" << endl;
+     cout << "Map 1: " << endl << endl
+            <<      "############################\n" 
+            <<      "#      *  #                #\n" 
+            <<      "# #   ######  #### ####### #\n" 
+            <<      "# #*   #       #    ##     #\n" 
+            <<      "# ###  #  ######*    #*  # #\n" 
+            <<      "#   *#     !########  #  # #\n" 
+            <<      "# ##  #     #      #  #  # #\n" 
+            <<      "# #*      *###  #  #  #  # #\n" 
+            <<      "# #########*    #  ####  # #\n" 
+            <<      "#            #  #*      *# #\n" 
+            <<      "############## *########## #\n"
+            <<      "#                          #\n"
+            <<      "#                          #\n"
+            <<      "#@                         #\n"
+            <<      "############################\n" << endl;
+}
+
+int timer(int min,int sec){
+    sec=0;
+    min=0;
+    for(sec=0;sec <=60;sec++){
+                  cout << min<< ":" << sec << endl;
+    if(sec == 60){           
+           min += 1;
+           sec=0;
     }
+        }
+                
+}
+
+//Function from Joshua Camacho don't credit for line count
+void clear(){
+
+#ifdef _WIN32
+	if ( hConsoleOutput == 0 )
+	{
+		hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+		GetConsoleScreenBufferInfo(hConsoleOutput, &csbi);
+	}
+#endif
+
+	 fflush(stdout);
+#ifdef _WIN32
+	{
+		COORD point;
+		DWORD dummy;
+		DWORD count;
+
+		point.X = 0;
+		point.Y = 0;
+		count	= csbi.dwSize.X * csbi.dwSize.Y;
+
+		FillConsoleOutputCharacter(hConsoleOutput, ' ', count, point, &dummy);
+		SetConsoleCursorPosition(hConsoleOutput, point);
+	}
+#else
+		 printf("\033[H\033[2J");
+#endif
+	 fflush(stdout);
 }
